0678ac66439cf3e8e78b84f18e76343e9bf4e9fe
suzanemuza123 <72234636+suzanemuza123@users.noreply.github.com>
2021-01-05 20:11:02 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function greet() {
+    alert ('Fresh flavors');
+}
+greet();
+
+try{
+    callExternalFunction();
+}
+catch(error) {
+    console.log(error.message);
+}
+
+var connection= getExternalConnection(); try {
+    connection.open();
+    connection.callExternalFunction();
+}
+catch(error){
+    console.log(error.message);
+}
